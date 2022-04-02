72040b2015b9cb5a1f1416338c44924c372cb7c3
CRUXsur <marcojtorrico@hotmail.com>
2022-04-02 17:16:11 -0400

    Backend: Socket: Emitir bandas registradas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,26 @@
 const { Server } = require('socket.io');
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
 
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
+console.log(bands);
 
 //Mensajes de Sockets
 io.on('connection', client =>{
     console.log('Cliente conectado');
+
+    //* cuando un cliente se conecta, yo puedo hacerle una emision de todas
+    //* las bandas que estan registradas actualmente en el servidor
+    //* asi como esten....y solo se las voy a emitir al cliente que se conecte
+    client.emit('active-bands', bands.getBands());
+
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
